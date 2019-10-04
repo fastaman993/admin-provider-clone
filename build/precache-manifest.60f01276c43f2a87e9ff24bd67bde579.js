7506570e10ebd76fd12e413cbfb48b2b36643953
Oreki13 <arfandynugraha21@gmail.com>
2019-10-04 07:40:17 +0700

    fixx sass

---
+++ build/precache-manifest.60f01276c43f2a87e9ff24bd67bde579.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcfbe938ac393c0cf093ca1631dd88ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42b98fc7da284d0da5d1",
+    "url": "/static/css/2.0b76d70b.chunk.css"
+  },
+  {
+    "revision": "3ac91925153859859386",
+    "url": "/static/css/main.b45ee0d0.chunk.css"
+  },
+  {
+    "revision": "42b98fc7da284d0da5d1",
+    "url": "/static/js/2.0cf2a897.chunk.js"
+  },
+  {
+    "revision": "3ac91925153859859386",
+    "url": "/static/js/main.6794ada0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
